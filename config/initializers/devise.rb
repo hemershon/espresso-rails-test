5fcca1aa04805473df2108af33af78867493e5c5
Hemershon Silva <hemesrhon@gmail.com>
2024-09-05 17:25:20 -0300

    feat create test for model category

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '539c3ba8ce056c020a22db8b42fe20a43a4f41e133253c832eee77f45c7a22c8cc4598f03b1dc6a8dc3cbbeb748ab6c63be9a3378a4547dcf100186697d0cb71'
+  # config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
